e2beae46a2b5b869bfcb2176567bef89ea366b1d
kobby08 <linusquam@gmail.com>
2022-04-18 17:03:30 +0000

    implemented line item date CRUD without turbo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :quotes do
-    resource :line_item_dates, except: %i[index show]
+    resources :line_item_dates, except: %i[index show]
   end
   root 'pages#home'
 end
